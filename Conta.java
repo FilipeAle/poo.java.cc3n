56c682d9c33bc28ccb648139aa1c220646e9ecbc
FilipeAle <filipinhoaleale@gmail.com>
2026-02-23 20:10:42 -0300

    Refactor Conta class with additional attributes

--- Conta.java
+++ Conta.java
@@ -2,22 +2,46 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.filipe20261902;
+package com.mycompany.filipe23022026;
 
 /**
  *
- * @author alunolab13
+ * @author alunolab01
  */
 public class Conta {
 
     private double saldo;
+    private int agencia, numero;
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
+
+    int getAgencia() {
+        return agencia;
+    }
+    
+    void setNumero(int numero){
+        this.numero = numero;
+    }
+    
+    int getNumero(){
+        return numero;
+    }
 
     Conta() {
 
     }
 
-    Conta(double valor) {
+    public Conta(int agencia, int numero, double valor) {
+        this.agencia = agencia;
+        this.numero = numero;
         creditar(valor);
+
     }
 
     public void creditar(double valor) {
@@ -27,14 +51,11 @@ public class Conta {
     public void debitar(double valor) {
         saldo = saldo - valor;
     }
-    
-    public void transferir(Conta destino, double valor){
+
+    public void transferir(Conta destino, double valor) {
         debitar(valor);
         destino.creditar(valor);
     }
 
-    public double obterSaldo() {
-        return saldo;
-    }
-
 }
+
