ad23a8282cce3b33dca4d3b3d280e4f8c453672e
FilipeAle <filipinhoaleale@gmail.com>
2026-02-23 20:10:18 -0300

    Update Principal class with new Conta initialization

--- Principal.java
+++ Principal.java
@@ -2,24 +2,27 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.filipe20261902;
+package com.mycompany.filipe23022026;
 
 /**
  *
- * @author alunolab13
+ * @author alunolab01
  */
 public class Principal {
 
     public static void main(String[] args) {
-        Conta c1 = new Conta(500);
+        Conta c1 = new Conta(123, 6630, 5000);
         c1.creditar(1000);
         c1.debitar(600);
         
         Conta c2 = new Conta();
-        c1.transferir(c2, 1000);
-        System.out.println("Saldo atual da conta 1: " + c1.obterSaldo()+"R$");
+        c2.setAgencia(124);
+        c2.setNumero(6631);
+        c2.creditar(15000);
+        c1.transferir(c2, 4000);
+        System.out.println("Saldo atual da conta 1: " + c1.getSaldo()+"R$");
         
-        System.out.println("Saldo atual da conta 2: " + c2.obterSaldo()+"R$");
+        System.out.println("Saldo atual da conta 2: " + c2.getSaldo()+"R$");
 
     }
 }
