474f490f250b5f18b0e5abfdf982b4e0a169461c
FilipeAle <filipinhoaleale@gmail.com>
2026-03-02 20:38:09 -0300

    Refactor Principal class and add user input
    Updated package name and modified main method to include user input for agency, account number, and balance. Added two Conta instances and performed a transfer operation.

--- filipe26022026 - Conta Bancaria/Principal.java
+++ filipe26022026 - Conta Bancaria/Principal.java
@@ -1,17 +1,38 @@
-                                                                                                                                                                                 /*
+/*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.filipe26022026;
-
+package com.mycompany.mavenproject1;
+import java.util.Scanner;
 /**
  *
- * @author alunolab13
+ * @author alunodev13
  */
 public class Principal {
-    Conta c1 = new Conta();
 
+   
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+    Scanner sc = new Scanner (System.in);
+    
+    System.out.println("Digite a agencia:");
+    int agencia = sc.nextInt();
+    
+    System.out.println("Digite a agencia:");
+    int numro = sc.nextInt();
+    
+    System.out.println("Digite a agencia:");
+    int saldo = sc.nextInt();
+    
+       
+        
+        
+    Conta c1 = new Conta(123, 999, 8000, 10000);
+    Conta c2 = new Conta(345, 888, 1000, 800);
+    
+    c1.transferir(c2, 10000);
+    
+        System.out.println("Saldo atual da conta 1: " + c1.getSaldo()+"R$");
+        System.out.println("Saldo atual da conta 2: " + c2.getSaldo()+"R$");
     }
 }
